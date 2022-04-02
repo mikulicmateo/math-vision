dc1b7afd9b723f80067384918188fba40273c2bf
Robin Šćulac <robin.sculac@gmail.com>
2022-04-02 15:16:32 +0200

    Dodavanje drugog acitvityia i njegovu klasu

---
+++ MathVisionAndroid/app/src/main/java/com/pi/math_vision_android/jednazdba_activity.java
@@ -0,0 +1,14 @@
+package com.pi.math_vision_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class jednazdba_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.jednadzba_layout);
+    }
+}
