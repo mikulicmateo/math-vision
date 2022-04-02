d92305305f8caf701860b277059ec10c36362bcd
patrikMedur01 <84334238+patrikMedur01@users.noreply.github.com>
2022-04-02 13:51:10 +0200

    Android project added

---
+++ MathVisionAndroid/app/src/main/java/com/pi/math_vision_android/MainActivity.java
@@ -0,0 +1,16 @@
+package com.pi.math_vision_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.e("hello", "hello world");
+    }
+}
