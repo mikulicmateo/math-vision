b74b371f9fdcd81716c5f76fcb97d2a3d084fe8b
Robin Šćulac <robin.sculac@gmail.com>
2022-04-02 15:36:09 +0200

    Dodao onclick i malo prilagodio margine

--- MathVisionAndroid/app/src/main/java/com/pi/math_vision_android/jednazdba_activity.java
+++ MathVisionAndroid/app/src/main/java/com/pi/math_vision_android/jednazdba_activity.java
@@ -2,7 +2,9 @@ package com.pi.math_vision_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class jednazdba_activity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class jednazdba_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.jednadzba_layout);
     }
+    public void ponovo(View v)
+    {
+        Intent namjera = new Intent(jednazdba_activity.this,MainActivity.class);
+        jednazdba_activity.this.startActivity(namjera);
+    }
+
 }
