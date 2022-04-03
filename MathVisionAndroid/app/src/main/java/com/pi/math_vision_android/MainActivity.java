bec70a86158c3d3ec2daa66a949bd91a42860f0b
patrikMedur01 <84334238+patrikMedur01@users.noreply.github.com>
2022-04-03 15:02:23 +0200

    Added functions for camera and permissions in AndroidManifest

--- MathVisionAndroid/app/src/main/java/com/pi/math_vision_android/MainActivity.java
+++ MathVisionAndroid/app/src/main/java/com/pi/math_vision_android/MainActivity.java
@@ -2,6 +2,9 @@ package com.pi.math_vision_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Camera;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -28,5 +31,29 @@ public class MainActivity extends AppCompatActivity {
         params.weight = 0.5f;
         layoutCamera.setLayoutParams(params);
 
+    }
+
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            //U open treba stavit camera ID
+           // c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
     }
 }
