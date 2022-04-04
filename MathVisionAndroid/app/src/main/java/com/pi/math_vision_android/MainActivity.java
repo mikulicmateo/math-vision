83db37afc755911a66959177d353f1b57d13431c
patrikMedur01 <84334238+patrikMedur01@users.noreply.github.com>
2022-04-04 20:57:07 +0200

    Added camera functionality in activity_main

--- MathVisionAndroid/app/src/main/java/com/pi/math_vision_android/MainActivity.java
+++ MathVisionAndroid/app/src/main/java/com/pi/math_vision_android/MainActivity.java
@@ -4,47 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.graphics.Camera;
+//import android.graphics.Camera;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.hardware.camera2.*;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Ovo je iz hardeare.camera
+    Camera camera;
+    FrameLayout cameraLayout;
+    ShowCamera showCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("hello", "hello world");
+        cameraLayout =(FrameLayout) findViewById(R.id.cameraLayout);
+
+        //Open back camera
+        camera = Camera.open();
+
+        showCamera = new ShowCamera(this, camera);
+        cameraLayout.addView(showCamera);
+
     }
+
+
+
 
     // This method calls when button "Take picture" is clicked
     public void clickTakePicture(View view) {
 
     }
 
-    /** Check if this device has a camera */
-    private boolean checkCameraHardware(Context context) {
-        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
-            // this device has a camera
-            return true;
-        } else {
-            // no camera on this device
-            return false;
-        }
-    }
-
-    /** A safe way to get an instance of the Camera object. */
-    public static Camera getCameraInstance(){
-        Camera c = null;
-        try {
-            //U open treba stavit camera ID
-           // c = Camera.open(); // attempt to get a Camera instance
-        }
-        catch (Exception e){
-            // Camera is not available (in use or does not exist)
-        }
-        return c; // returns null if camera is unavailable
-    }
 }
