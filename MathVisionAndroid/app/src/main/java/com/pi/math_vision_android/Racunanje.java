b04a2f8dbdc5266be5c2360752d4de3596c69e00
Robin Šćulac <robin.sculac@gmail.com>
2022-04-05 18:08:04 +0200

    Dodana AAR, klasa za simboličnu matematiku.

---
+++ MathVisionAndroid/app/src/main/java/com/pi/math_vision_android/Racunanje.java
@@ -0,0 +1,31 @@
+package com.pi.math_vision_android;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.matheclipse.core.eval.EvalUtilities;
+import org.matheclipse.core.interfaces.IExpr;
+import org.matheclipse.parser.client.SyntaxError;
+import org.matheclipse.parser.client.math.MathException;
+
+public class Racunanje {
+    public String getAnswer(String s) {
+
+        EvalUtilities util = new EvalUtilities(false,true);
+        try{
+            IExpr result = util.evaluate(s);
+            return result.toString();
+        }
+        catch (SyntaxError e){
+            Log.e("err",e.getMessage());
+            return "Syntax error";
+        }
+        catch (MathException e){
+            Log.e("err",e.getMessage());
+            return "Math Error";
+        }
+
+    }
+
+}
