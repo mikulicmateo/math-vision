90ea38acf2a74412614758e8da806f2d2d537cf0
patrikMedur01 <84334238+patrikMedur01@users.noreply.github.com>
2022-04-06 18:44:21 +0200

    Added functions for button to take pictures

--- MathVisionAndroid/app/src/main/java/com/pi/math_vision_android/MainActivity.java
+++ MathVisionAndroid/app/src/main/java/com/pi/math_vision_android/MainActivity.java
@@ -3,13 +3,21 @@ package com.pi.math_vision_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 //import android.graphics.Camera;
 import android.hardware.Camera;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,15 +37,63 @@ public class MainActivity extends AppCompatActivity {
 
         showCamera = new ShowCamera(this, camera);
         cameraLayout.addView(showCamera);
+        galleryAddPic();
 
     }
 
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(Environment.getExternalStorageDirectory().getPath() + File.separator + "imageDirectory");
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    Camera.PictureCallback mPictureCallback = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] bytes, Camera camera) {
+            File picture_file = getOutputMediaFile();
+
+            if(picture_file  == null){
+                return;
+            }else {
+                try {
+                    FileOutputStream fos = new FileOutputStream(picture_file);
+                    fos.write(bytes);
+                    fos.close();
+
+                    camera.startPreview();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    };
+
+    private File getOutputMediaFile() {
+        String state = Environment.getExternalStorageState();
+        if(!state.equals(Environment.MEDIA_MOUNTED)){
+            return null;
+        }else{
+            File folder_image = new File(Environment.getExternalStorageDirectory().getPath() + File.separator + "imageDirectory");
+            if(!folder_image.exists()){
+                folder_image.mkdirs();
+            }
+
+            File outputFile = new File(folder_image,"temp.jpg");
+            return outputFile;
+        }
+    }
 
 
 
     // This method calls when button "Take picture" is clicked
     public void clickTakePicture(View view) {
-
+        if(camera != null){
+            camera.takePicture(null,null,mPictureCallback);
+        }
     }
 
 }
--- MathVisionAndroid/app/src/main/java/com/pi/math_vision_android/ShowCamera.java
+++ MathVisionAndroid/app/src/main/java/com/pi/math_vision_android/ShowCamera.java
@@ -7,6 +7,7 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import java.io.IOException;
+import java.util.List;
 
 public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
     Camera camera;
@@ -32,19 +33,22 @@ public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
     public void surfaceCreated(SurfaceHolder holder){
         Camera.Parameters params = camera.getParameters();
 
-        //Change the orientation of the camera
+        //Get camera picture size
+        List<Camera.Size> sizes = params.getSupportedPictureSizes();
+        Camera.Size mSize = null;
 
+        for(Camera.Size size : sizes){
+            mSize = size;
+        }
+
+        //Change the orientation of the camera
         if(this.getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE){
             params.set("orientation", "portrait");
             camera.setDisplayOrientation(90);
             params.setRotation(90);
         }
-        else{
-            params.set("orientation","landscape");
-            camera.setDisplayOrientation(0);
-            params.setRotation(0);
-        }
 
+        params.setPictureSize(mSize.width, mSize.height);
         camera.setParameters(params);
         try {
             camera.setPreviewDisplay(holder);
