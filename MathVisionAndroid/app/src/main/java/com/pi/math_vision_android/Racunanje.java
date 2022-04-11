1b68c912deb2b7ddc2ab9f87d326859a66b2ea68
Robin Šćulac <robin.sculac@gmail.com>
2022-04-11 20:31:43 +0200

    popravak JQmath sintakse

--- MathVisionAndroid/app/src/main/java/com/pi/math_vision_android/Racunanje.java
+++ MathVisionAndroid/app/src/main/java/com/pi/math_vision_android/Racunanje.java
@@ -10,13 +10,18 @@ import org.matheclipse.core.interfaces.IExpr;
 import org.matheclipse.parser.client.SyntaxError;
 import org.matheclipse.parser.client.math.MathException;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import jp.ac.kobe_u.cs.cream.Solution;
+
 public class Racunanje {
     public String getAnswer(String s) {
 
         EvalUtilities util = new EvalUtilities(false,true);
         try{
             IExpr result = util.evaluate(s);
-            return result.toString();
+            return"$$"+ stringToJqmath(result.toString())+"$$</br>"+"testing string";
         }
         catch (SyntaxError e){
             Log.e("err",e.getMessage());
@@ -25,6 +30,15 @@ public class Racunanje {
         catch (MathException e){
             return "Math Error";
         }
+
+    }
+    public String stringToJqmath(String solution)  {
+        String prvaZamjena=solution.replaceAll("Sqrt","√");
+        String DrugaZamjena=prvaZamjena.replaceAll("Pow","^");
+        String trecaZamjena=DrugaZamjena.replaceAll("\\{", "" );
+        String Cetvrtazamjena=trecaZamjena.replaceAll("\\}", "" );
+
+        return Cetvrtazamjena.replaceAll(",","\\$\\$\\$\\$");
 
     }
 
--- MathVisionAndroid/app/src/main/java/com/pi/math_vision_android/jednazdba_activity.java
+++ MathVisionAndroid/app/src/main/java/com/pi/math_vision_android/jednazdba_activity.java
@@ -15,8 +15,10 @@ public class jednazdba_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.e("rest","test");
         setContentView(R.layout.jednadzba_layout);
+        Racunanje calc = new Racunanje();
         MathView mvTest = (MathView) findViewById(R.id.MathView);
-        mvTest.setBackgroundColor("$\\F&#x2196{&#x2192}=ma_g$");
+        ;
+        mvTest.setText("riješenje jednadžbe:"+"$${x^2=4,x+y^2=6}$$ je "+calc.getAnswer("Solve({x^2==4,x+y^2==6}, {x,y})"));//testni kod zamjeni se varijablama kada ga budemo imali
 
 
     }
