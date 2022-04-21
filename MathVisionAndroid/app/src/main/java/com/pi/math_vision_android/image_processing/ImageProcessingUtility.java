ac79222b61f59633a6d569371ec7741d2aff6709
Mateo Mikulić <mikulic.dev@gmail.com>
2022-04-21 20:36:56 +0200

    Made augment image method and structured basic methods that need developing

--- MathVisionAndroid/app/src/main/java/com/pi/math_vision_android/image_processing/ImageProcessingUtility.java
+++ MathVisionAndroid/app/src/main/java/com/pi/math_vision_android/image_processing/ImageProcessingUtility.java
@@ -15,26 +15,39 @@ import java.util.List;
 
 public class ImageProcessingUtility {
 
+
+    public Mat preprocessImage(){
+
+        return null;
+    }
+
     private Mat readImage(Bitmap bitmap){
         Imgcodecs imageCodecs = new Imgcodecs();
-        imageCodecs.imread(); // =
-        Mat mat = new Mat()
+//        imageCodecs.imread(); // =
+        Mat mat = new Mat();
         Utils.bitmapToMat(bitmap, mat);
 
-        Imgproc.cvtColor(mat, mat, Imgproc.COLOR_BGR2GRAY); //grayscale
-        Imgproc.blur(mat,mat, new Size(5,5));
-        Imgproc.threshold(mat, mat, 125, 250, Imgproc.THRESH_BINARY_INV); // thresholding
-
-        Mat rectKernel = Imgproc.getStructuringElement(Imgproc.CV_SHAPE_RECT, new Size(2,2));
-        Imgproc.dilate(mat, mat, rectKernel, new Point(-1,-1), 3); //dilation
 
         List<MatOfPoint> contours = new ArrayList<>();
         Mat hierarchy = new Mat();
         Imgproc.findContours(mat, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE); //find contours
 
 
-
+        return null;
     }
+
+    private Mat augmentImage(Mat mat){
+        Imgproc.cvtColor(mat, mat, Imgproc.COLOR_BGR2GRAY); //grayscale
+        Imgproc.blur(mat,mat, new Size(5,5));
+        Imgproc.threshold(mat, mat, 125, 250, Imgproc.THRESH_BINARY_INV); // thresholding
+        Mat rectKernel = Imgproc.getStructuringElement(Imgproc.CV_SHAPE_RECT, new Size(2,2));
+        Imgproc.dilate(mat, mat, rectKernel, new Point(-1,-1), 3); //dilation
+
+        return mat;
+    }
+
+
+
 
 
 }
